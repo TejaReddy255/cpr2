74421ed2a2e35f875034dc5521d9f8ae2e797e32
TejaReddy255 <39689987+TejaReddy255@users.noreply.github.com>
2022-04-21 16:34:32 +0530

    compeleted login system

---
+++ app.py
@@ -0,0 +1,5 @@
+from sqlalchemy import true
+from cpr import app
+ 
+if  __name__=='__main__':
+     app.run(debug=True)
---
+++ cpr/form.py
@@ -0,0 +1,65 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
+from cpr.models import User
+
+class RegistrationFrom(FlaskForm):
+    username=StringField('Username',
+                                   validators=[DataRequired(),Length(min=2,max=(20))])
+    email=StringField('Email',validators=[DataRequired(),Email()]) 
+
+    password=PasswordField('Password',validators=[DataRequired()])
+
+    confirm_password=PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    
+    submit=SubmitField('Sign up')
+    def validate_username(self,user):
+        user=User.query.filter_by(username=user.data).first()
+        if user:
+            raise ValidationError('username is taken.choose different one.')
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email is taken.choose different one.')
+
+class LoginFrom(FlaskForm):
+   
+    email=StringField('Email',validators=[DataRequired(),Email()]) 
+
+    password=PasswordField('Password',validators=[DataRequired()])
+
+    remember=BooleanField('Remeber Me')
+    
+    submit=SubmitField('Login')
+
+class RecruterRegistrationFrom(FlaskForm):
+    companyname=StringField('Company name',
+                                   validators=[DataRequired(),Length(min=2,max=(40))])
+    username=StringField('Username',
+                                   validators=[DataRequired(),Length(min=2,max=(20))])
+    email=StringField('Email',validators=[DataRequired(),Email()]) 
+
+    password=PasswordField('Password',validators=[DataRequired()])
+
+    confirm_password=PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    
+    submit=SubmitField('Sign up')
+    def validate_username(self,user):
+        user=User.query.filter_by(username=user.data).first()
+        if user:
+            raise ValidationError('username is taken.choose different one.')
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email is taken.choose different one.')
+
+class RecruterLoginFrom(FlaskForm):
+   
+    email=StringField('Email',validators=[DataRequired(),Email()]) 
+
+    password=PasswordField('Password',validators=[DataRequired()])
+
+    remember=BooleanField('Remeber Me')
+    
+    submit=SubmitField('Login')
+                                    
